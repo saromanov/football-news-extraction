90cb7e3e49e8eb787e63560c114807b15c63f0b2
Sergey Romanov <xxsmotur@gmail.com>
2015-07-29 09:33:07 -0700

    First commit

---
+++ example.py
@@ -0,0 +1,5 @@
+import model
+
+text = 'Norwich complete Bready deal. Platini to stand Fifa president. Jose Mourinho says Rafael Benitez wife should "take care or her husband diet" rather than critique the Chelsea boss management'
+item = model.Model(text)
+item.run()
---
+++ model.py
@@ -0,0 +1,21 @@
+import nltk
+
+class Model:
+    def __init__(self, text):
+        self.text = text
+
+    def _stopwords(self):
+        return nltk.corpus.stopwords.words('english')
+
+    def _tagging(self):
+        pass
+
+    def run(self):
+        if len(self.text) == 0:
+            raise Exception("Text is empty")
+
+        for sent in nltk.sent_tokenize(self.text):
+            tokenizer = [word for word in nltk.word_tokenize(sent) if word not in self._stopwords()]
+            tagging = nltk.pos_tag(tokenizer)
+            for chunk in nltk.ne_chunk(tagging):
+                print(chunk)
